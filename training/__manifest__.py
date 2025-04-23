e384e1164b59806ed8a4794de1012a314b823225
Jaafar Ahmed <jaafarahmed4937@gmail.com>
2025-04-23 12:24:17 +0300

    Add files via upload

---
+++ training/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Training",
+
+    'summary': "Training for employees",
+
+    'web_icon':'training/static/description/icon.png',
+
+    'description': """
+        Training for employees ...
+    """,
+
+    'author': "Jaffar",
+    
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base',
+        'hr',
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/course_seq.xml',
+        'views/my_courses_view.xml',
+        'views/registration_seq.xml',
+        'views/room_view.xml',
+        'views/location_view.xml',
+        'views/course_view.xml',
+        'views/registration_view.xml',
+        'views/teacher_view.xml',
+        'report/registration_report.xml',
+        'views/menu.xml'
+    ],
+}
+
---
+++ training/models/location.py
@@ -0,0 +1,28 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+class TrainingLocation(models.Model):
+    _name = 'training.location'
+    _description = 'Location model'
+
+    name = fields.Char(string='Location Name', required=True)
+    code = fields.Char(string="Location Code", required=True)
+
+    @api.constrains('name', 'code')
+    def _check_unique_name_code(self):
+        for record in self:
+            # check name uniquene
+            existing_name = self.search([
+                ('name', '=', record.name),
+                ('id', '!=', record.id)
+            ], limit=1)
+            if existing_name:
+                raise ValidationError(_("The location name '%s' must be unique.") % record.name)
+
+            # check code uniquene
+            existing_code = self.search([
+                ('code', '=', record.code),
+                ('id', '!=', record.id)
+            ], limit=1)
+            if existing_code:
+                raise ValidationError(_("The location code '%s' must be unique.") % record.code)
